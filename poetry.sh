5ae462bd70d74be5278b0a121de3e8aa1dc5af3c
Kronosol <45532845+blipk@users.noreply.github.com>
2023-03-20 22:59:36 +1100

    bumpver

--- poetry.sh
+++ poetry.sh
@@ -1,5 +1,6 @@
 # poetry init   # create config
 # poetry shell  # create venv
 # poetry add <package> # add packagers
-# poetry -v build --format=wheel
+poetry build
+poetry -v build --format=wheel
 # poetry publish
