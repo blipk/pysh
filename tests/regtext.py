43b44c11b7be7475e32fc0941f07c9da49d27abf
Kronosol <45532845+blipk@users.noreply.github.com>
2023-03-20 17:13:51 +1100

    update doc

--- tests/regtext.py
+++ tests/regtext.py
@@ -1,25 +1,33 @@
-import sys
 from pysh import pysh
 pysh(__file__)
+
+#$@ echo "simple"
+
+#$@!sh echo "simple"
+#$%@!sh echo "simple"
+
+#$!@sh echo
+#$@%!sh echo
+#$@% echo
+
+#$!sh echo "simple"
 
 #$ echo "Pysh activated"
 stdout = ""#$ echo "This is standard out"
 
 
-###### General syntax
 
 # Python comments
 #$  ls .         # pysh eol comment
 ##$ sudo rm -rf  # disable pysh line
 
-# run external script with double $
-#$$ my_script.sh
+#$ cd $HOME
 
-# optionally pass arguments to it
+#$$ my_script.sh
 #$$ argumentative_script.sh arg1 arg2
 
-# Change the shell that interprets the script
-# must support -c command_strings or filepath if external $$
+#$@sh echo "simple"
+
 #$!sh echo "simple"
 #$!perl oysters.pl
 #$$!bash script.sh
@@ -28,10 +36,6 @@ stdout = ""#$!python import time; print("The time and date", time.asctime())
 if __name__ == "main":
     print("Before the above script blocks are run")
 
-###### Multiple inline pysh
-
-# Block 0
-#$ cd $HOME
 
 stdout_block1 = ""#$ echo "first block is multiline"
 #$ echo "line1"
@@ -43,11 +47,6 @@ stdout_block2 == ""#$ echo "Second"
 #$ echo "Block"
 
 
-##### Real usage
-
-# Script your system in paralel with your python code execution
-# Do anything you can in bash e.g.
-#$ firefox https://news.ycombinator.com && echo "opened link in firefox"
 
 build_service()
 #$ cd ~/hosted/myservice && docker compose up
@@ -55,8 +54,6 @@ build_service()
 aggregate_assets()
 fmpg_result = ""#$ ffmpeg -i raw_video.mkv -crf {{crf}} -o
 process_assets(process_fmpg_stdout(fmpg_result))
-
-
 
 pysher.shyp()
 #$ echo "pysh enabled"
